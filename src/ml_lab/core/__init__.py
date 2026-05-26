2ea37481eacf7d50af085afe9ac975b1c272f52b
Paul Skeffington <36854171+pskeffington@users.noreply.github.com>
2026-05-26 00:54:06 -0400

    Add core package exports

---
+++ src/ml_lab/core/__init__.py
@@ -0,0 +1,18 @@
+"""Core domain objects for adaptive scaffolding experiments."""
+
+from ml_lab.core.actions import ScaffoldAction
+from ml_lab.core.arms import ExperimentArm
+from ml_lab.core.metrics import CalibrationMetric, HintDependencyMetric, OutcomeMetric
+from ml_lab.core.policies import RuleBasedScaffoldPolicy, ScaffoldPolicy
+from ml_lab.core.state import LearnerState
+
+__all__ = [
+    "CalibrationMetric",
+    "ExperimentArm",
+    "HintDependencyMetric",
+    "LearnerState",
+    "OutcomeMetric",
+    "RuleBasedScaffoldPolicy",
+    "ScaffoldAction",
+    "ScaffoldPolicy",
+]
