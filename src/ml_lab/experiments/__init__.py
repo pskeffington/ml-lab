0d151a82d7af3b1e33af3aee56625747d2209b4a
Paul Skeffington <36854171+pskeffington@users.noreply.github.com>
2026-05-26 00:59:19 -0400

    Add experiments package

---
+++ src/ml_lab/experiments/__init__.py
@@ -0,0 +1,5 @@
+"""Experiment orchestration utilities for ml-lab."""
+
+from ml_lab.experiments.runner import ExperimentRunner, SyntheticLearner, TaskEnvironment
+
+__all__ = ["ExperimentRunner", "SyntheticLearner", "TaskEnvironment"]
